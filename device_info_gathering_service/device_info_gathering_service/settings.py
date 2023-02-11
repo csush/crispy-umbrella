c3f70d005697c94f6e221b03a4f6447794b462ea
Sushant Chaudhary <csush.95@gmail.com>
2023-02-11 16:44:28 +0100

    updated settings

--- device_info_gathering_service/device_info_gathering_service/settings.py
+++ device_info_gathering_service/device_info_gathering_service/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'device_info_gathering_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crispyumbrella', 
+        'USER': 'crispyumbrella',
+        'PASSWORD': 'crispyumbrella',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
