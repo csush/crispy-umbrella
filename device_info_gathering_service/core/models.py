64168e0010ae014e7d81701b7d5d89cf1cd11139
Sushant Chaudhary <csush.95@gmail.com>
2023-02-11 16:44:36 +0100

    added company model

--- device_info_gathering_service/core/models.py
+++ device_info_gathering_service/core/models.py
@@ -1,3 +1,40 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class BaseModel(models.Model):
+    id = models.UUIDField(
+        verbose_name=_('UUID'),
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+
+    created_at = models.DateTimeField(
+        verbose_name=_('Created at'),
+        auto_now_add=True,
+        editable=False,
+    )
+
+    updated_at = models.DateTimeField(
+        verbose_name=_('Updated at'),
+        auto_now=True,
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Company(BaseModel):
+    name = models.CharField(
+        verbose_name=_('Name'),
+        max_length=100,
+        unique=True,
+    )
+
+    location = models.CharField(
+        verbose_name=_('Location'),
+        max_length=100,
+    )
